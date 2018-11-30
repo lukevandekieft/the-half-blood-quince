f7b25587493507c7973190f4a667781cbfb2b5c9
Luke Vandekieft <vandekie@gmail.com>
2018-11-30 14:54:56 -0800

    Set up webpack environment.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+//styles
+import './App.scss';
+import sassStyles from './test-sass.module.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div><p>Hi</p></div>
+    );
+  }
+}
+
+export default App;
