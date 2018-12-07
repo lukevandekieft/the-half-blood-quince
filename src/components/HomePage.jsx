95d555fdc6d947858940b89971fef7f01d068112
Luke Vandekieft <vandekie@gmail.com>
2018-12-07 15:36:02 -0800

    Update navbar and header styles.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,12 +3,24 @@ import RecipeList from './RecipeList';
 import NavButton from './NavButton';
 
 function HomePage(){
+
+  let headerStyles = {
+    backgroundImage: 'url(https://images.unsplash.com/photo-1516211697506-8360dbcfe9a4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=52e95cdd2653761514253e73eeb938ac&auto=format&fit=crop&w=1500&q=80)',
+    height: '50vh',
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    position: 'relative',
+  };
   return (
-    <div className='pageContentSection'>
-    <p>Current Recipes</p>
-      <RecipeList />
-      <NavButton />
-      <NavButton />
+    <div className='homePageContainer'>
+      <div style={headerStyles}></div>
+      <div className='pageContentSection'>
+        <p>Current Recipes</p>
+          <RecipeList />
+          <NavButton />
+          <NavButton />
+      </div>
     </div>
   );
 }
