5eed0b694c403b50480e28587fea5cc37a5711e5
Luke Vandekieft <vandekie@gmail.com>
2018-12-21 14:26:42 -0800

    Set up edit recipe form.

--- src/components/EditRecipeForm.jsx
+++ src/components/EditRecipeForm.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import NavButton from './NavButton';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-function EditRecipeForm(){
+function EditRecipeForm({currentRecipe, name}){
+  console.log(name)
   return (
     <div>
       <form>
+        <label>Recipe Name</label>
+        <input type="text" defaultValue={currentRecipe}></input>
+        <label>Recipe Link</label>
+        <input type="text"></input>
+        <label>Recipe Picture (link to picture)</label>
+        <input type="text"></input>
+        <label>Ingredients</label>
+        <textarea></textarea>
+        <label>Ingredient Notes</label>
+        <textarea></textarea>
+        <label>Directions</label>
+        <textarea></textarea>
+        <label>Direction Notes</label>
+        <textarea></textarea>
         <button type="submit" className='navButtonStyle'>Submit</button>
-        <NavButton
-          linkPath='/'
-          linkText='Cancel Changes'
-        />
       </form>
+      <NavButton
+      linkPath='/'
+      linkText='Cancel Changes'
+      />
     </div>
   );
 }
 
-export default EditRecipeForm;
+const mapStateToProps = state => {
+  return {
+    currentRecipe: state.currentRecipeId,
+    name: state.recipes[state.currentRecipeId].name
+  };
+};
+
+export default connect(mapStateToProps)(EditRecipeForm);
