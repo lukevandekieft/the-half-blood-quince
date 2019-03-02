93c1cd8d2d77c9cb26519e05e91de43c13d9f390
Luke Vandekieft <vandekie@gmail.com>
2019-03-02 12:32:20 -0800

    Import base styles for modal component.

---
+++ src/components/Widgets/MenuModal/AccordionItem.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AccordionItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accordionOpen: false,
+    }
+    this.toggleAccordion = this.toggleAccordion.bind(this);
+  }
+
+  toggleAccordion() {
+    this.setState(({ accordionOpen }) => ({ accordionOpen: !accordionOpen }));
+  }
+
+  componentDidUpdate (prevProps) {
+    if (prevProps !== this.props) {
+      if (!this.props.modalShowing && this.state.accordionOpen) {
+        this.toggleAccordion();
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.accordionOpen ? 'accordion-item accordion-open' : 'accordion-item'}>
+        <a className ='accordion-label' onClick={this.toggleAccordion}>
+          <h3>{this.props.name}</h3>
+          <i className={this.state.accordionOpen ? 'material-icons menu-arrow view-icon' : 'material-icons menu-arrow'}>arrow_drop_down_circle</i>
+        </a>
+        <div className='accordion-submenus'>
+          {this.props.submenus.map(index => {
+            return (
+              <a key={index}>{index}</a>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+
+  static propTypes = {
+    name: PropTypes.string,
+  }
+}
+
+export default AccordionItem;
--- src/components/Widgets/MenuModal/MenuModal.jsx
+++ src/components/Widgets/MenuModal/MenuModal.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { newUserLogout } from '../../../actions';
 
+import AccordionItem from './AccordionItem';
+
 class MenuModal extends React.Component {
   render () {
 
@@ -12,7 +14,27 @@ class MenuModal extends React.Component {
 
     return (
       <div className='modalMenu'>
-        <button className='loginLogout' onClick={handleLogout}>Logout</button>
+        <div className={this.props.modalShowing ? 'modal accordion modal-open-style' : 'modal accordion'}>
+          <div className='modal-top'>
+            <i className='material-icons' onClick={this.props.onToggleModal}>close</i>
+          </div>
+          <div className='modal-body'>
+            <div className='modal-container'>
+              <div className='accordion-list'>
+                    <AccordionItem
+                      name = 'what up'
+                      submenus = {['hi', 'hello']}
+                      modalShowing = {this.props.modalShowing}
+                    />
+                  <div className='accordion-footer'>
+                    <p>Lorem ipsum dolor sit?</p>
+                    <p>Ras sit amet neque arcu. Vivamus faucibus in sapien sit amet dignissim.</p>
+                    <a className='btn btn-secondary'>Clickum Ipset</a>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
