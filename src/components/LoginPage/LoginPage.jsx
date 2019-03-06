1ce8ca996eecbb485cc4e6f0981caa7f0db6513b
Luke Vandekieft <vandekie@gmail.com>
2019-03-06 00:04:59 -0800

    Add google sign-in button to Login Page.

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { newUserLogin } from './../../actions';
 
 import Loader from '../Widgets/Loader/Loader';
 
 class Login extends React.Component{
   render() {
+
+    const handleGoogleLogin = () => {
+      this.props.dispatch(newUserLogin());
+    };
+
     return (
       <div>
-      <div className='headerSection'>
-      </div>
-      Please Log In
+        <div className='headerSection'></div>
+        <p>Please Log In</p>
+        <button className='loginLogout' onClick={handleGoogleLogin}>Login with Google</button>
       </div>
     );
   }
