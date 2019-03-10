5b4dfcc3e308e8ff3c7a3510866a3a43a257b581
Luke Vandekieft <vandekie@gmail.com>
2019-03-09 21:44:08 -0800

    Further improve login form styles.

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -44,12 +44,13 @@ class Login extends React.Component{
           <div className='navBarMargin'></div>
           <div className='loginBox'>
             <div className= 'loginSection'>
-              <h2>Whoops - This Page is Half-baked!</h2>
-              <p>Thankfully the rest of the site is up and running. Click below to view a test account with dummy data!</p>
+              <h2>Welcome to Half-Blood Quince!</h2>
+              <p>Feel free to look around on our demo account below:</p>
               <button className='loginButton demo' onClick={() =>{handleLogin({email: 'ramb222@yahoo.com', password: 'testpassword'})}}>View Demo</button>
             </div>
 
             <div className='loginSection'>
+            <h3>Sign in Below</h3>
             <button className='loginButton google' onClick={() =>{handleLogin('google')}}><img className='logo' src={google} alt='Google Logo'/>Log in with Google</button>
             <button className='loginButton facebook' onClick={() =>{handleLogin('facebook')}}><img className='logo' src={facebook} alt='Facebook Logo'/>Log in with Facebook</button>
             <div className='loginDivider'>
